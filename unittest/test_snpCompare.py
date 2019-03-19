e118f7a287a71654bc43ec5a78b8d840addd360b
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-03-19 14:01:45 +0000

    script update

--- unittest/test_snpCompare.py
+++ unittest/test_snpCompare.py
@@ -4,24 +4,53 @@ import os, sys
 import unittest, vcf
 
 rootPath=os.path.dirname(os.path.abspath(sys.argv[0])) + "/.."
-sourcePath=rootPath + "/scripts"
+sourcePath=rootPath + "/snpfc"
 sys.path.append(sourcePath)
 
-from snpCompare import Compare
+from snpCompare import snpcompare
 
 
 class test_Compare(unittest.TestCase):
 
-	test1 = rootPath + "/testfiles/test1.vcf"
-	test2 = rootPath + "/testfiles/test2.vcf"
-	test3 = rootPath + "/testfiles/test3.vcf"
+	test1 = rootPath + "/testfiles/unittest1.vcf"
+	test2 = rootPath + "/testfiles/unittest2.vcf"
+	docompare = snpcompare([test1, test2])
 
-	def test_compare(self):
+	def test_record_all_snp_positions(self):
+		self.docompare.record_all_snp_positions('chr1', 100)
+		self.assertEqual(self.docompare.snpsites, {'chr1': {'775': [True, True], '776': [False, True], '100': [False, False]}})
 
-		docompare = Compare([self.test1, self.test2, self.test3])
+	def test_record_all_snps(self):
+		self.docompare.record_all_snps(self.test1, 'chr1', 100, 'A', 'G')
+		self.assertEqual(self.docompare.snp_positions, {
+				self.test1: {'chr1': {'100': {'ref':'A', 'alt':'G'}, '775': {'ref': 'A', 'alt': 'G'} } },
+				self.test2: {'chr1': {'775': {'ref':'A', 'alt':'G'},  '776': {'ref': 'T', 'alt': 'C'} } }
+				})
+
+	def test_get_snp_data(self):
+		self.assertEqual(self.docompare.get_snp_data(), None)
+	def test_count_list_elements_occurrences(self):
+		self.assertEqual(self.docompare.count_list_elements_occurrences(['A', 'T','A','C','G', 'G']), [2,1,2,1,2,2])
+	def test_get_unique_snps(self):
+		print(self.docompare.snpsites)
+		print(self.docompare.snp_positions)
+		self.docompare.get_snp_data()
+		print(self.docompare.snpsites)
+		self.docompare.get_unique_snps()
+		print(self.docompare.snp_positions)
+		self.assertEqual(self.docompare.snp_positions, {
+				self.test1: {'chr1': {'775': {'ref': 'A', 'alt': 'G'}}},
+				self.test2: {'chr1': {'775': {'ref':'A', 'alt':'G'},  '776': {'ref': 'T', 'alt': 'C', 'unique':True} } }
+				}  )
 
 
-		self.assertEqual(docompare.compare(rootPath + "/testfiles"), None)
+	#
+	# def test_compare(self):
+	#
+	# 	docompare = Compare([self.test1, self.test2, self.test3])
+	#
+	#
+	# 	self.assertEqual(docompare.compare(rootPath + "/testfiles"), None)
 
 
 
